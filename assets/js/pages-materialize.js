375b9df46fb2a9c3bf6d5c0ec9b79ddb42cfb2c2
Chad Kruse <chadokruse@users.noreply.github.com>
2019-11-27 13:08:46 -0600

    Add simple layout using Materialize

---
+++ assets/js/pages-materialize.js
@@ -0,0 +1,13 @@
+---
+---
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+
+});
