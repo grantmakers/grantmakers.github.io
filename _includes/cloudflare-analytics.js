48dd12ff4ee720b27371cfaa9a9fdea4e76d6388
Chad Kruse <chadokruse@users.noreply.github.com>
2020-12-12 13:43:22 -0600

    Add Cloudflare analytics as possible ga replacement

---
+++ _includes/cloudflare-analytics.js
@@ -0,0 +1,3 @@
+<!-- Cloudflare Web Analytics -->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "370802ed80eb452faad5ac648390ef30"}'></script>
+<!-- End Cloudflare Web Analytics -->
